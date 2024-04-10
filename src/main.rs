4e2c77943a35552585d8f94294d1126a5ec0b014
final-gaLaxy <sebastian@genus.net.au>
2024-04-10 22:41:57 +1000

    create a window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+extern crate glium;
+extern crate winit;
+
 fn main() {
-    println!("Hello, world!");
+    let event_loop = winit::event_loop::EventLoopBuilder::new().build().expect("event loop building");
+
+    let (_window, display) = glium::backend::glutin::SimpleWindowBuilder::new().build(&event_loop);
+
+    let _ = event_loop.run(move |event, window_target| {
+        match event {
+            winit::event::Event::WindowEvent { event, .. } => match event {
+                winit::event::WindowEvent::CloseRequested => window_target.exit(),
+                _ => (),
+            },
+            _ => (),
+        };
+    });
 }
